e84165ad0d3ef72f5a47d2b9613d8ffb76af50c6
Faical sawadogo <faical@faicals-mbp.home>
2019-01-27 15:43:01 -0500

    Initial Commit

---
+++ Music Player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by Faical sawadogo on 1/27/19.
+//  Copyright © 2019 Faical sawadogo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
